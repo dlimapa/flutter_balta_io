eed1f9fe095ec97765faaf32006130d335e78170
Daniel de Lima <dlima@tre-pa.jus.br>
2020-05-02 01:19:11 -0300

    Fim da aula 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_balta_io/model/item.dart';
 
 void main() => runApp(App());
 
@@ -16,30 +17,38 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo List'),
-        leading: Text('leading'),
-        actions: <Widget>[
-          Icon(
-            Icons.plus_one,
-          ),
-        ],
-      ),
-      body: Container(
-        child: Center(
-          child: Text(
-            'App',
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 30,
+        appBar: AppBar(
+          title: Text('Todo List'),
+          leading: Text('leading'),
+          actions: <Widget>[
+            Icon(
+              Icons.plus_one,
             ),
-          ),
+          ],
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            return Text(widget.items[index].title);
+          },
+        ));
   }
 }
